9dcf51851207edfd6d80f9bca4d31385bef930cd
João Pinto <lamego.pinto@gmail.com>
2020-08-16 19:44:23 +0200

    fix cli test

--- tests/test_create.py
+++ tests/test_create.py
@@ -1,19 +1,20 @@
-from pyroot.project import Project
+from pytest import fail
 from tempfile import TemporaryDirectory
 from shutil import rmtree
+from pyroot.project import Project
 
 
 def test_project_create():
 
     with TemporaryDirectory() as proj_dir:
-        # Must fail when creating one xisting dir
+        # Must fail when creating with existing dir
         proj = Project("test_project", proj_dir)
         try:
             proj.create()
         except SystemExit:
             pass
         else:
-            raise Exception
+            fail("did not exist on existying dir")
         proj = Project("test_project", proj_dir)
         # Should continue if force is used
         proj.create(force=True)
